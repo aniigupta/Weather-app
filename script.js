a1494a44cecf72eaa91144ef65fc10b566216895
Aniket <Aniket8076206264@gmail.com>
2023-12-24 17:44:23 +0530

    s

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
-const apikey = "6f32ee98d1672d788fe5c291f90787de";
-const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-// https://api.openweathermap.org/data/2.5/weather?q=london&appid=6f32ee98d1672d788fe5c291f90787de
+const apiKey = "6f32ee98d1672d788fe5c291f90787de";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const searchBox =document.querySelector(".search input");
-const searchBtn =document.querySelector(".search button");
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkweather(city){
-    const response =await fetch(apiurl+city+`&appid=${apikey}`);
-    var data = await response.json();
-    console.log(data);
-    document.querySelector(".city").innerHTML=data.name;
-    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
-    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
-    document.querySelector(".wind").innerHTML=data.wind.speed+ "km/h";
+async function checkWeather(city) {
+    try {
+        if (!city) {
+            alert("Please enter a city name");
+            return;
+        }
 
-    if(data.weather[0].main=="clouds"){
-        weatherIcon.src ="images/clouds.png";
+        const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log(data);
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+            const weatherMain = data.weather[0].main.toLowerCase();
+            setWeatherIcon(weatherMain);
+        } else {
+            console.error("Error fetching weather data:", data.message);
+            alert("Error fetching weather data. Please try again.");
+        }
+    } catch (error) {
+        console.error("An unexpected error occurred:", error);
+        alert("An unexpected error occurred. Please try again.");
     }
-    else if(data.weather[0].main=="Rain"){
-        weatherIcon.src ="images/rain.png";
-    }
-    else if(data.weather[0].main=="Drizzle"){
-        weatherIcon.src ="images/drizzle.png";
-    }
-    else if(data.weather[0].main=="Mist"){
-        weatherIcon.src ="images/mist.png";
-    }
-    // document.querySelector(".weather").style.display="block";
 }
-searchBtn.addEventListener("click",()=>{
-    
-    checkweather(searchBox.value);
-})
 
+function setWeatherIcon(weatherMain) {
+    switch (weatherMain) {
+        case "clouds":
+            weatherIcon.src = "images/clouds.png";
+            break;
+        case "rain":
+            weatherIcon.src = "images/rain.png";
+            break;
+        case "drizzle":
+            weatherIcon.src = "images/drizzle.png";
+            break;
+        case "mist":
+            weatherIcon.src = "images/mist.png";
+            break;
+        default:
+            weatherIcon.src = ""; // Set a default image or leave it blank
+    }
+}
 
-
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
